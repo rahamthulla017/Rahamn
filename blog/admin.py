27c471dbe6ffd82f7a617cf4a80d55c2a4344589
rahamthulla017 <146557967+rahamthulla017@users.noreply.github.com>
2024-02-23 22:41:00 +0530

    Delete blog directory

--- blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Post
-# Register your models here.
-@admin.register(Post)
-class PostModelAdmin(admin.ModelAdmin):
- list_display = ['id', 'title', 'desc']
